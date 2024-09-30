68b72c2ae97845b9e2d319432afb03ecfabd43b7
Syed-Haider-Shah <haidershah123123123@gmail.com>
2024-09-30 22:24:40 +1000

    Searches based on month as well as search term

--- read.py
+++ read.py
@@ -1,12 +1,15 @@
 import csv
 import re
 
-search_term = input("Please enter a search term: ")
-pattern = fr"(?=.*{search_term})"
+search_term_1 = input("Month ")
+search_term_2 = input("Search ")
+pattern1 = fr"(?=.*{search_term_1})"
+pattern2 = fr"(?=.*{search_term_2})"
 
-with open('data.csv', 'r') as csv_file:
+with open('data2.csv', 'r') as csv_file:
   csv_reader = csv.reader(csv_file)
 
   for line in csv_reader:
-    if re.match(pattern, line[2]):
-      print(line[2])
+    if re.match(pattern1, line[0]):
+      if re.match(pattern2, line[2]):
+        print(line[2], line[1])
