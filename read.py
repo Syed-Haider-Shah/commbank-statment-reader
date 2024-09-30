22ce6d2fb8eede5f7a9fc1f862c3acf6f3a4a1c8
Syed-Haider-Shah <haidershah123123123@gmail.com>
2024-09-30 22:29:39 +1000

    Stored the result in array

--- read.py
+++ read.py
@@ -6,10 +6,14 @@ search_term_2 = input("Search ")
 pattern1 = fr"(?=.*{search_term_1})"
 pattern2 = fr"(?=.*{search_term_2})"
 
+results = []
+
 with open('data2.csv', 'r') as csv_file:
   csv_reader = csv.reader(csv_file)
 
   for line in csv_reader:
     if re.match(pattern1, line[0]):
       if re.match(pattern2, line[2]):
-        print(line[2], line[1])
+        results.append((line[2], line[1]))
+
+print(results)
