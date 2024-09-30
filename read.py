9df69e226cba1f0e9cf444aa61eac44f4982a180
Syed-Haider-Shah <haidershah123123123@gmail.com>
2024-09-30 21:34:22 +1000

    pattern matching successful

--- read.py
+++ read.py
@@ -1,7 +1,11 @@
 import csv
+import re
+
+pattern = r"(?=.*\bTransfer\b)"
 
 with open('data.csv', 'r') as csv_file:
   csv_reader = csv.reader(csv_file)
 
   for line in csv_reader:
-    print(line)
+    if re.match(pattern, line[2]):
+      print(line[2])
