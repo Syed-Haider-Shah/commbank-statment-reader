c59c566eaa9dd097ac5dee0b5f9f62e5834dc1be
Syed-Haider-Shah <haidershah123123123@gmail.com>
2024-10-01 17:34:37 +1000

    cors setup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__) #instance of an app
+cors = CORS(app, origins='*') #change this later
 
 @app.route("/api/users", methods=['GET'])
 def users():
